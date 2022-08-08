1c58af4c3ae8a83559fbf563c5a2db906f18f00b
Deepak Goswami <lodalelemome@gmail.com>
2022-08-07 23:53:13 -0700

    kurti

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const ejs=require("ejs");
+const path=require("path");
+const port=process.env.PORT || 3000;
+const app=express();
+const staticpath=path.join(__dirname,"/public");
+app.use(express.static(staticpath))
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+app.get("/buynow",(req,res)=>{
+    res.render("buynow");
+})
+app.listen(port,()=>{
+    console.log("Connected...");
+})
